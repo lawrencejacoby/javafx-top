9a11b92ff43cda79f8be06f9e63bd361965d3fcc
Alexander Wahl <alexecommerce@gmx.de>
2026-03-29 14:51:58 +0200

    circle

---
+++ src/main/java/de/wahl/javafx/top/FxTopApp.java
@@ -0,0 +1,57 @@
+package de.wahl.javafx.top;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class FxTopApp extends Application{
+		
+		public static void main(String[] args) {
+			System.setProperty("prism.order", "sw");
+		    System.setProperty("glass.gtk.disableAppIndicator", "true");			
+			launch(args);
+		}
+
+		private double xOffset = 0;
+	    private double yOffset = 0;
+
+	    @Override
+	    public void start(Stage primaryStage) {
+	        // 1. Der Kreis
+	        Circle circle = new Circle(100, Color.CORAL);
+	        //circle.setStroke(Color.BLACK);
+	        //circle.setStrokeWidth(5);
+
+	        // 2. Container mit fixer Mindestgröße
+	        StackPane root = new StackPane(circle);
+	        root.setPrefSize(300, 300);
+	        // Zum Testen: Ändere TRANSPARENT zu RED, um zu sehen ob das Fenster da ist
+	        root.setStyle("-fx-background-color: transparent;"); 
+
+	        // 3. Drag-and-Drop Logik (damit man es bewegen kann)
+	        root.setOnMousePressed(event -> {
+	            xOffset = event.getSceneX();
+	            yOffset = event.getSceneY();
+	        });
+	        root.setOnMouseDragged(event -> {
+	            primaryStage.setX(event.getScreenX() - xOffset);
+	            primaryStage.setY(event.getScreenY() - yOffset);
+	        });
+
+	        // 4. Scene & Stage
+	        Scene scene = new Scene(root, 300, 300);
+	        scene.setFill(Color.TRANSPARENT); // WICHTIG
+
+	        primaryStage.initStyle(StageStyle.TRANSPARENT); // WICHTIG
+	        primaryStage.setOpacity(0.99);
+	        primaryStage.setScene(scene);
+	        primaryStage.setAlwaysOnTop(true);
+	        primaryStage.show();
+	        primaryStage.requestFocus(); // Erzwinge den Fokus
+	    }
+
+}
